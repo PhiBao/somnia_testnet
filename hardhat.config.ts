82e0dec71558d753e37e0ed2940556a053892390
PhiBao <kiter0211@gmail.com>
2025-03-22 21:15:30 +0700

    feat(deploy): contratc on somnia devnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,17 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    somnia: {
+      url: process.env.SOMNIA_RPC_URL,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
 };
 
 export default config;
